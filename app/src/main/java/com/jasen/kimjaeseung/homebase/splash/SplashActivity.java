38428f9012ebe84008bd6bd49e0118a9348ea692
silver432 <pub60@hanmail.net>
2018-01-06 16:44:02 +0900

    fixed untracked files

---
+++ app/src/main/java/com/jasen/kimjaeseung/homebase/splash/SplashActivity.java
@@ -0,0 +1,18 @@
+package com.jasen.kimjaeseung.homebase.splash;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by kimjaeseung on 2018. 1. 6..
+ */
+
+public class SplashActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
