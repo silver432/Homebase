16765ebeefdcd585ca5bd883effba768d2fe2f8e
silver432 <pub60@hanmail.net>
2018-01-06 16:56:32 +0900

    add splash

--- app/src/main/java/com/jasen/kimjaeseung/homebase/splash/SplashActivity.java
+++ app/src/main/java/com/jasen/kimjaeseung/homebase/splash/SplashActivity.java
@@ -1,8 +1,11 @@
 package com.jasen.kimjaeseung.homebase.splash;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+
+import com.jasen.kimjaeseung.homebase.main.MainActivity;
 
 /**
  * Created by kimjaeseung on 2018. 1. 6..
@@ -14,5 +17,10 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+
+        finish();
     }
 }
